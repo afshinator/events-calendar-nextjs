e5129b25d06f7d38aae049f5e1dd361f03a1e0b4
Afshin Mokhtari <afshinator@gmail.com>
2024-12-10 15:08:36 -0800

    Add click handlers for adding and deleting events

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,6 +1,76 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+import {
+  formatDate,
+  DateSelectArg,
+  EventClickArg,
+  EventApi,
+} from "@fullcalendar/core";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
 
 const Calendar = () => {
+  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const [newEventTitle, setNewEventTitle] = useState<string>("");
+  const [selectedDate, setSelectedDate] = useState<DateSelectArg | null>(null);
+
+
+  const handleDateClick = (selected: DateSelectArg) => {
+    setSelectedDate(selected);
+    setIsDialogOpen(true);
+  };
+
+
+  const handleEventClick = (selected: EventClickArg) => {
+    // Prompt user for confirmation before deleting an event
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event "${selected.event.title}"?`
+      )
+    ) {
+      selected.event.remove();
+    }
+  };
+
+
+
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+    setNewEventTitle("");
+  };
+
+  const handleAddEvent = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (newEventTitle && selectedDate) {
+      const calendarApi = selectedDate.view.calendar; // Get the calendar API instance.
+      calendarApi.unselect(); // Unselect the date range.
+
+      const newEvent = {
+        id: `${selectedDate.start.toISOString()}-${newEventTitle}`,
+        title: newEventTitle,
+        start: selectedDate.start,
+        end: selectedDate.end,
+        allDay: selectedDate.allDay,
+      };
+
+      calendarApi.addEvent(newEvent);
+      handleCloseDialog();
+    }
+  };
+
   return (<div>calnedar here</div>)
 }
 
